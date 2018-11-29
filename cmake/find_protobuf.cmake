9d8e4e5044553f1efce2934870118db26a5258e0
Kenji Noguchi <tokyo246@gmail.com>
2018-11-29 18:58:15 +0000

    ...

--- cmake/find_protobuf.cmake
+++ cmake/find_protobuf.cmake
@@ -9,13 +9,12 @@ INCLUDE_DIRECTORIES(${PROTOBUF_INCLUDE_DIR})
 set (USE_PROTOBUF 1)
 
 #[[TODO:
-option (USE_INTERNAL_RDKAFKA_LIBRARY "Set to FALSE to use system libprotobuf instead of the bundled" ${NOT_UNBUNDLED})
+option (USE_INTERNAL_PROTOBUF_LIBRARY "Set to FALSE to use system libprotobuf instead of the bundled" ${NOT_UNBUNDLED})
 
 endif ()
 
 if (USE_PROTOBUF)
     message (STATUS "Using protobuf=${USE_PROTOBUF}: ${PROTOBUF_INCLUDE_DIR} : ${PROTOBUF_LIBS}")
 else ()
-    message (STATUS "Build without capnp (support for Cap'n Proto format will be disabled)")]]
-
+    message (STATUS "Build without Protobuf (support for Protobuf format will be disabled)")]]
 endif ()
